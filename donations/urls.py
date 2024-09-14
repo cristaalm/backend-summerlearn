3fad8adf170e2db5cce47780ffd20c02024916cc
Alfredo <elizaldiromero14@gmail.com>
2024-09-13 20:00:40 -0600

    add: donations views and bill view

---
+++ donations/urls.py
@@ -0,0 +1,11 @@
+from rest_framework.routers import DefaultRouter
+from donations.views import DonationsViews, BillsViews
+from django.urls import path, include
+
+
+router = DefaultRouter()
+
+router.register(r'donations', DonationsViews, basename='donations')
+router.register(r'bills', BillsViews, basename='bills')
+
+urlpatterns = router.urls
