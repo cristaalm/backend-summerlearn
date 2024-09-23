7e0d3a5de7f2ce8ad41b62aaa700046b00f8b583
gaboard <gabovalencia2009@gmail.com>
2024-09-23 13:07:49 -0600

    HANDMADE MERGE3.0CAWN

--- myApp/urls.py
+++ myApp/urls.py
@@ -20,10 +20,16 @@ urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'), 
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    
+
+    # Custom API Root: Para poder cargar la vista personalizada del API Root en la URL raíz del proyecto (http://localhost:8000/)
+    path('', CustomAPIRootView.as_view(), name='api-root'),
+
+    # Incluir las URLs de otras apps: Seguir añadiendo las URLs de las otras apps
     path('', include('programs_activities.urls')),
     path('', include('schedules.urls')),
     path('', include('donations.urls')),
     path('', include('children.urls')),
-    path('', include(router.urls)),  # Incluye las URLs del router
+
+    # Incluir las URLs del router al final
+    path('', include(router.urls)),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
