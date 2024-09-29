b046161ce7164427158541f149923d47955687b5
Eduarduar <eduardoarcega1997@gmail.com>
2024-09-29 00:50:11 -0600

    Refactor donations/urls.py to add PDF export functionality for bills

--- donations/urls.py
+++ donations/urls.py
@@ -2,7 +2,6 @@ from rest_framework.routers import DefaultRouter
 from donations.views import DonationsViews, BillsViews
 from django.urls import path, include
 
-
 router = DefaultRouter()
 
 router.register(r'donations', DonationsViews, basename='donations')
@@ -10,5 +9,6 @@ router.register(r'bills', BillsViews, basename='bills')
 
 urlpatterns = [
     path('', include(router.urls)),  # Rutas CRUD para Donations 
-    path('bills/exportar-excel/', BillsViews.as_view({'get': 'exportar_bills_excel'}), name='exportar_bills_excel'), 
+    path('bills/exportar-excel/', BillsViews.as_view({'get': 'exportar_bills_excel'}), name='exportar_bills_excel'),
+    path('bills/exportar-pdf/', BillsViews.as_view({'get': 'exportar_bills_pdf'}), name='exportar_bills_pdf'),
 ]
