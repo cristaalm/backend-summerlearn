591c97c8f07ad8e092211ad1226670fc4e7c5198
gaboard <gabovalencia2009@gmail.com>
2024-10-10 09:49:38 -0600

    Hotfix: Representation performance_beneficiaries

--- performance_beneficiaries/serializers.py
+++ performance_beneficiaries/serializers.py
@@ -12,7 +12,8 @@ class PerformanceBeneficiariesSerializer(serializers.ModelSerializer):
         representation = super().to_representation(instance)
         representation['performance_beneficiaries_subscription'] = {
             'id': instance.performance_beneficiaries_subscription.subscriptions_children_id,
-            'name': instance.performance_beneficiaries_subscription.subscriptions_children_activity.activities_name,
+            'name': instance.performance_beneficiaries_subscription.subscriptions_children_child.children_name,
+            'description': instance.performance_beneficiaries_subscription.subscriptions_children_activity.activities_name,
         }
         return representation
 
