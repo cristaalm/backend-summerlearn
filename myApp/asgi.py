f2bba6c86b99e5e904b1bdb9502761d5551a0f89
Eduarduar <eduardoarcega1997@gmail.com>
2024-10-15 11:26:06 -0600

    Refactor asgi.py to add WebSocket support for chat functionality

--- myApp/asgi.py
+++ myApp/asgi.py
@@ -1,7 +1,16 @@
 import os
-
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+import myApp.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myApp.settings')
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            myApp.routing.websocket_urlpatterns
+        )
+    ),
+})
