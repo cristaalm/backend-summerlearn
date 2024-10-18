4eedea3e1e05468f8aafa802b3133f8e3d9d9096
Eduarduar <eduardoarcega1997@gmail.com>
2024-10-18 07:11:23 -0600

    Refactor Chat model to change chat_id field to CharField

--- myApp/models.py
+++ myApp/models.py
@@ -224,7 +224,7 @@ class Activities(models.Model):
         db_table = 'activities'
 
 class Chat(models.Model):
-    chat_id = models.BigAutoField(primary_key=True)
+    chat_id = models.CharField(max_length=255, primary_key=True, unique=True)
     chat_date = models.DateField()
     chat_user1 = models.ForeignKey('UserData', models.DO_NOTHING, db_column='chat_user1', related_name='chats_as_user1')
     chat_user2 = models.ForeignKey('UserData', models.DO_NOTHING, db_column='chat_user2', related_name='chats_as_user2')
