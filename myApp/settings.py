14ff8f61bf57a8638af9febbe30f6165319b9fef
Eduarduar <eduardoarcega1997@gmail.com>
2024-10-24 07:18:44 -0600

    Refactor database configuration in settings.py

--- myApp/settings.py
+++ myApp/settings.py
@@ -113,6 +113,14 @@ DATABASES = {
     }
 }
 
+# DB LOCAL
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
