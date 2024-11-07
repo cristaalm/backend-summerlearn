b8c3d9e557d8abe1b0a1c4ad192520d2e89aa7a9
gaboard <gabovalencia2009@gmail.com>
2024-11-07 11:38:23 -0600

    add: inserción de la imagen del child

--- children/views.py
+++ children/views.py
@@ -1,12 +1,22 @@
+from django.db import IntegrityError
+from django.core.files.storage import default_storage
+import os
+import uuid
+
+############################################
+
+from myApp.models import Children
+############################################
+
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework import viewsets
 from rest_framework import permissions
 from rest_framework_simplejwt.authentication import JWTAuthentication
-from myApp.models import Children
 from .serializers import ChildrensSerializer
-
-
+from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny
+from rest_framework.status import HTTP_400_BAD_REQUEST, HTTP_404_NOT_FOUND
 
 class ChildrensViewSet(viewsets.ModelViewSet):
     queryset = Children.objects.all()
@@ -15,8 +25,20 @@ class ChildrensViewSet(viewsets.ModelViewSet):
     authentication_classes = [JWTAuthentication]
 
     def create(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
 
+        # obtenemos la imagen del request
+        children_photo = request.data.get('children_photo', None)
+
+        # Creamos un nombre de archivo único
+        filename = f"{uuid.uuid4()}.jpg"
+        # Juntamos el path con el nombre del archivo
+        path = os.path.join('childrenImages', filename)
+        # Guardamos la imagen en el directorio
+        default_storage.save(path, children_photo)
+        # Reemplazamos el campo children_photo con la ruta de la imagen
+        request.data['children_photo'] = f'media/childrenImages/{filename}'
+
+        serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
 
         self.perform_create(serializer)
